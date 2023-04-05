c062697109ab72cdf61c34ff4a19b80fe722bb08
Marcin Bialek <test-git@youcode.pl>
2023-04-05 14:44:55 +0200

    added a style css to img

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import "./App.css"
 
 function App() {
 
-const [result, setResult] = useState();
+  const [result, setResult] = useState();
 
-const calculateResult = (currency, amount) => {
+  const calculateResult = (currency, amount) => {
     const rate = currencies.find(({ short }) => short === currency).rate;
 
     setResult({
       sourceAmount: +amount,
-      targetAmount: amount / rate,
+      targetAmount: +amount / rate,
       currency,
     })
   }
--- src/FildsetCurrency/index.js
+++ src/FildsetCurrency/index.js
@@ -6,35 +6,35 @@ export const FildsetCurrency = ({title}) => (
         <legend className="fildset__legend">{title}</legend>
         <p className="fildset__paragraph">
             <label className="fildset__label">
-                <img  width="40" height="35" src="https://i.postimg.cc/BZRTNctr/flagi-unii-europejskie.png" alt="Flaga Unii"/>
+                <img className="img" width="40" height="30" src="https://i.postimg.cc/CxsDpsmL/Flaga-Unii.png" alt="Flaga Unii"/>
                     <span className="fildset__currency">Euro (EUR): <strong > 4.67</strong></span>
             </label>
-            <label className="fildset__label"><img  width="40" height="35" src="https://i.postimg.cc/qR5RJL94/flaga-norwegii.jpg" alt="Flaga Norwegii"/>
+            <label className="fildset__label"><img className="img" width="40" height="30" src="https://i.postimg.cc/RhwhQvYV/FLaga-Norwegii.png" alt="Flaga Norwegii"/>
             <span
                 className="fildset__currency" >Korona norweska (NOK): <strong >0.41</strong> </span>
             </label>
         </p>
         <p className="fildset__paragraph">
-            <label className="fildset__label"><img  width="40" height="35" src="https://i.postimg.cc/8PWhYJ6j/flaga-usa.jpg" alt="Flaga USA"/>
+            <label className="fildset__label"><img className="img"  width="40" height="30" src="https://i.postimg.cc/5tHxSxYd/Flaga-USA.png" alt="Flaga USA"/>
                 <span className="fildset__currency">Dolar amerykański (USD): <strong > 4.30</strong></span>
             </label>
-            <label className="fildset__label"><img  width="40" height="35" src="https://i.postimg.cc/T15Ppb6m/flaga-kanady.jpg" alt="Flaga Kanady" />
+            <label className="fildset__label"><img className="img" width="40" height="30" src="https://i.postimg.cc/0NcTDRgC/Flaga-Kanady.png" alt="Flaga Kanady" />
                 <span className="fildset__currency">Dolar kanadyjski (CAD): <strong >3.16</strong> </span>
             </label>
         </p>
         <p className="fildset__paragraph">
-            <label className="fildset__label"><img  width="40" height="35" src="https://i.postimg.cc/kM2NxhMN/flaga-brytyjska.jpg" alt="Flaga Wiwlkiej Brytanii" />
+            <label className="fildset__label"><img className="img" width="40" height="30" src="https://i.postimg.cc/kM2NxhMN/flaga-brytyjska.jpg" alt="Flaga Wiwlkiej Brytanii" />
                 <span className="fildset__currency">Funt brytyjski (GBR): <strong >5.31</strong> </span>
             </label>
-            <label className="fildset__label"><img  width="40" height="35" src="https://i.postimg.cc/LXX5F7Qx/flaga-japonii.jpg" alt="Flaga Japonii"/>
+            <label className="fildset__label"><img className="img" width="40" height="30" src="https://i.postimg.cc/LXX5F7Qx/flaga-japonii.jpg" alt="Flaga Japonii"/>
                 <span className="fildset__currency">Jen (JPY): <strong >0.03</strong> </span>
             </label>
         </p>
         <p className="fildset__paragraph">
-            <label className="fildset__label"><img  width="40" height="35" src="https://i.postimg.cc/wjphYqXF/flaga-szwajcarii.jpg" alt="Flaga Szwajcarii" />
+            <label className="fildset__label"><img className="img" width="40" height="30" src="https://i.postimg.cc/DZwj1B1k/Flaga-Szwajcarii.png" alt="Flaga Szwajcarii" />
                 <span className="fildset__currency">Frank szwajcarski (CHF): <strong >4.68</strong> </span>
             </label>
-            <label className="fildset__label"><img  width="40" height="35" src="https://i.postimg.cc/JtwzJhrF/flaga-czech.jpg" alt="Flaga Czech"/>
+            <label className="fildset__label"><img className="img" width="40" height="30" src="https://i.postimg.cc/8k0pJYLG/Flaga-Czech.png" alt="Flaga Czech"/>
                 <span className="fildset__currency">Korona czeska (CZK): <strong >0.20</strong> </span>
             </label>
         </p>
